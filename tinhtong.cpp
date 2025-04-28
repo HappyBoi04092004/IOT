985c55f716a60fbb6f5335f1b16bffe3479beb4c
HappyBoi04092004 <163809990+HappyBoi04092004@users.noreply.github.com>
2025-04-28 20:47:51 +0700

    tong

---
+++ tinhtong.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2;
+    cout << "Nhập số thứ nhất: ";
+    cin >> num1;
+    cout << "Nhập số thứ hai: ";
+    cin >> num2;
+    int sum = num1 + num2;
+    cout << "Tổng của hai số là: " << sum << endl;
+    return 0;
+}
