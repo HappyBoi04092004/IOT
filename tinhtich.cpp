747ba9c87e1a9b9c41fdf5992f79627a3ede0b6e
HappyBoi04092004 <163809990+HappyBoi04092004@users.noreply.github.com>
2025-04-28 20:17:36 +0700

    Tinh tich cua 2 so

---
+++ tinhtich.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2;
+    cout << "Nhập số thứ nhất: ";
+    cin >> num1;
+    
+    cout << "Nhập số thứ hai: ";
+    cin >> num2;
+    int product = num1 * num2;
+    cout << "Tích của hai số là: " << product << endl;
+    return 0;
+}
