318f3a29099d0a59e60617fb6c18b042264b43f2
HappyBoi04092004 <163809990+HappyBoi04092004@users.noreply.github.com>
2025-04-28 20:29:10 +0700

    tich

---
+++ Tinhtich.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2;
+    cout << "Nhập số thứ nhất: ";
+    cin >> num1;
+    cout << "Nhập số thứ hai: ";
+    cin >> num2;
+    int product = num1 * num2;
+    ccout << "Tích của hai số là: " << product << endl;
+    return 0;
+}
